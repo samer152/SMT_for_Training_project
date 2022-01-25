b9a09f719d659c297e98e57d04f77c1c35b869ea
Tuval198 <tuvalgelvan@campus.technion.ac.il>
2022-01-25 19:57:15 +0200

    custom matmul support.

--- custom_matmul.py
+++ custom_matmul.py
@@ -5,6 +5,10 @@ from torch.nn.utils import weight_norm
 
 from ADQ_example import ADQ_Example
 from BF16_matmul import BF16Matmul
+from BF9_matmul import BF9Matmul
+from BF13_matmul import BF13Matmul
+from BF11_matmul import BF11Matmul
+from BF24_matmul import BF24Matmul
 from NormalMatmul import NormalMatmul
 from utils import Dtype, Stream, load_kernel, Dtype_size
 
@@ -14,9 +18,21 @@ def custom_matmul(input, weight, compute_flavour):
         # NORMAL matmul
         return NormalMatmul.apply(input, weight)
     elif compute_flavour == 2:
-        # BF16
-        return BF16Matmul.apply(input, weight)
+        # Mantissa 0
+        return BF9Matmul.apply(input, weight)
     elif compute_flavour == 3:
+        # Mantissa 2
+        return BF11Matmul.apply(input, weight)
+    elif compute_flavour == 4:
+        # Mantissa 4
+        return BF13Matmul.apply(input, weight)
+    elif compute_flavour == 5:
+        # BF16 - Mantissa 7
+        return BF16Matmul.apply(input, weight)
+    elif compute_flavour == 6:
+        # Mantissa 15
+        return BF24Matmul.apply(input, weight)
+    elif compute_flavour == 7:
         # ADQ example with zero 1 element
         return ADQ_Example.apply(input, weight)
     else:
