2e72a0c5148b4d903bd2e4598d9a9a25114bb2dc
Idan-Golan <idan.go@campus.technion.ac.il>
2022-02-12 17:01:52 +0200

    Add run scripts for resnet & alexnet with cifar100

---
+++ run_alexnet.sh
@@ -0,0 +1,8 @@
+python3 main.py --action TRAINING --arch alexnet-cifar100 --epoch 75 --batch_size 128 --seed 44 --gpu 1 --compute_flavour 1 --v 1
+python3 main.py --action TRAINING --arch alexnet-cifar100 --epoch 75 --batch_size 128 --seed 44 --gpu 1 --compute_flavour 2 --v 1
+python3 main.py --action TRAINING --arch alexnet-cifar100 --epoch 75 --batch_size 128 --seed 44 --gpu 1 --compute_flavour 4 --v 1
+python3 main.py --action TRAINING --arch alexnet-cifar100 --epoch 75 --batch_size 128 --seed 44 --gpu 1 --compute_flavour 5 --v 1
+python3 main.py --action TRAINING --arch alexnet-cifar100 --epoch 75 --batch_size 128 --seed 44 --gpu 1 --compute_flavour 6 --v 1
+python3 main.py --action TRAINING --arch alexnet-cifar100 --epoch 75 --batch_size 128 --seed 44 --gpu 1 --compute_flavour 7 --v 1
+python3 main.py --action TRAINING --arch alexnet-cifar100 --epoch 75 --batch_size 128 --seed 44 --gpu 1 --compute_flavour 8 --v 1
+python3 main.py --action TRAINING --arch alexnet-cifar100 --epoch 75 --batch_size 128 --seed 44 --gpu 1 --compute_flavour 9 --v 1
---
+++ run_resnet.sh
@@ -0,0 +1,4 @@
+python3 main.py --action TRAINING --arch resnet18-cifar100 --epoch 100 --batch_size 128 --seed 44 --gpu 0 --compute_flavour 5 --v 1
+python3 main.py --action TRAINING --arch resnet18-cifar100 --epoch 100 --batch_size 128 --seed 44 --gpu 0 --compute_flavour 6 --v 1
+python3 main.py --action TRAINING --arch resnet18-cifar100 --epoch 100 --batch_size 128 --seed 44 --gpu 0 --compute_flavour 7 --v 1
+python3 main.py --action TRAINING --arch resnet18-cifar100 --epoch 100 --batch_size 128 --seed 44 --gpu 0 --compute_flavour 8 --v 1
