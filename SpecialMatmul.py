b12f3257ef023b4319ca6b1e612821b7c6a71f88
Idan Golan <IdanGolan7@gmail.com>
2022-02-12 13:29:04 +0200

    Add BF9matmul - empty mantissa

--- SpecialMatmul.py
+++ SpecialMatmul.py
@@ -170,3 +170,31 @@ class PXR24Matmul(Function):
             weights_gradients = grad_output_new.matmul(inputs_pxr24)
             weights_gradients = weights_gradients.sum(0)
             return inputs_gradients, weights_gradients
+
+class BF9Matmul(Function):
+    @staticmethod
+    def forward(ctx, inputs, weights):
+        ctx.save_for_backward(inputs, weights)
+
+        # convert to BF9
+        weights_bf9 = convert_to_bf9(weights)
+        inputs_bf9 = convert_to_bf9(inputs)
+
+        weights_bf9 = weights_bf9.t().contiguous()
+        return inputs_bf9.matmul(weights_bf9)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        with torch.no_grad():
+
+            inputs, weights = ctx.saved_tensors
+
+            weights_bf9 = convert_to_bf9(weights)
+            inputs_bf9 = convert_to_bf9(inputs)
+            grad_output_bf9 = convert_to_bf9(grad_output.contiguous())
+
+            inputs_gradients = grad_output_bf9.matmul(weights_bf9)
+            grad_output_new = grad_output_bf9.transpose(1,2).contiguous()
+            weights_gradients = grad_output_new.matmul(inputs_bf9)
+            weights_gradients = weights_gradients.sum(0)
+            return inputs_gradients, weights_gradients
