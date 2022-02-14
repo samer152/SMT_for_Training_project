c7f4471cf844bf6974b16cee175bac186f715122
Idan-Golan <62376268+Idan-Golan@users.noreply.github.com>
2022-02-14 16:42:59 +0200

    Add run scripts for converted sums.

---
+++ run_alexnet_converted_sum.sh
@@ -0,0 +1,7 @@
+python3 main.py --action TRAINING --arch alexnet-cifar100 --epoch 100 --batch_size 128 --seed 44 --gpu 1 --compute_flavour 10 --v 1
+python3 main.py --action TRAINING --arch alexnet-cifar100 --epoch 100 --batch_size 128 --seed 44 --gpu 1 --compute_flavour 11 --v 1
+python3 main.py --action TRAINING --arch alexnet-cifar100 --epoch 100 --batch_size 128 --seed 44 --gpu 1 --compute_flavour 12 --v 1
+python3 main.py --action TRAINING --arch alexnet-cifar100 --epoch 100 --batch_size 128 --seed 44 --gpu 1 --compute_flavour 13 --v 1
+python3 main.py --action TRAINING --arch alexnet-cifar100 --epoch 100 --batch_size 128 --seed 44 --gpu 1 --compute_flavour 14 --v 1
+python3 main.py --action TRAINING --arch alexnet-cifar100 --epoch 100 --batch_size 128 --seed 44 --gpu 1 --compute_flavour 15 --v 1
+python3 main.py --action TRAINING --arch alexnet-cifar100 --epoch 100 --batch_size 128 --seed 44 --gpu 1 --compute_flavour 16 --v 1
---
+++ run_resnet_converted_sum.sh
@@ -0,0 +1,7 @@
+python3 main.py --action TRAINING --arch resnet18-cifar100 --epoch 100 --batch_size 128 --seed 44 --gpu 0 --compute_flavour 10 --v 1
+python3 main.py --action TRAINING --arch resnet18-cifar100 --epoch 100 --batch_size 128 --seed 44 --gpu 0 --compute_flavour 11 --v 1
+python3 main.py --action TRAINING --arch resnet18-cifar100 --epoch 100 --batch_size 128 --seed 44 --gpu 0 --compute_flavour 12 --v 1
+python3 main.py --action TRAINING --arch resnet18-cifar100 --epoch 100 --batch_size 128 --seed 44 --gpu 0 --compute_flavour 13 --v 1
+python3 main.py --action TRAINING --arch resnet18-cifar100 --epoch 100 --batch_size 128 --seed 44 --gpu 0 --compute_flavour 14 --v 1
+python3 main.py --action TRAINING --arch resnet18-cifar100 --epoch 100 --batch_size 128 --seed 44 --gpu 0 --compute_flavour 15 --v 1
+python3 main.py --action TRAINING --arch resnet18-cifar100 --epoch 100 --batch_size 128 --seed 44 --gpu 0 --compute_flavour 16 --v 1
