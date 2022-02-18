46a39a5d2c7060a95948ef91d38f0588c7c51e56
Idan-Golan <idan.go@campus.technion.ac.il>
2022-02-18 19:03:48 +0200

    Add Asym training results.

---
+++ run_alexnet_asym.sh
@@ -0,0 +1,6 @@
+python3 main.py --action TRAINING --arch alexnet-cifar100 --epoch 100 --batch_size 128 --seed 44 --gpu 1 --compute_flavour 20 --v 1
+python3 main.py --action TRAINING --arch alexnet-cifar100 --epoch 100 --batch_size 128 --seed 44 --gpu 1 --compute_flavour 21 --v 1
+python3 main.py --action TRAINING --arch alexnet-cifar100 --epoch 100 --batch_size 128 --seed 44 --gpu 1 --compute_flavour 22 --v 1
+python3 main.py --action TRAINING --arch alexnet-cifar100 --epoch 100 --batch_size 128 --seed 44 --gpu 1 --compute_flavour 23 --v 1
+python3 main.py --action TRAINING --arch alexnet-cifar100 --epoch 100 --batch_size 128 --seed 44 --gpu 1 --compute_flavour 24 --v 1
+python3 main.py --action TRAINING --arch alexnet-cifar100 --epoch 100 --batch_size 128 --seed 44 --gpu 1 --compute_flavour 25 --v 1
