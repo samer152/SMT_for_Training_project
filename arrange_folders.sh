172dc4b0bb5ccb0bb830d579f3a4ddfcc339d0f9
Tuval198 <tuvalgelvan@campus.technion.ac.il>
2022-02-22 12:57:41 +0200

    arrange folders update

--- arrange_folders.sh
+++ arrange_folders.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 
+#echo "make folders"
+#for f in *baseline_results*; do
+#  echo $f
+#  echo "${f//\\//}"
+#  if [ "$f" != '*.log' ]
+#  then
+#    mkdir -p "${f//\\//}"
+#  fi
+#  mv -- "$f"/* "${f//\\//}"
+#done
+#
+#echo "move log files"
+#for f in *baseline_results*.log*; do
+#  echo $f
+#  mv -- "$f" "${f//\\//}"
+#done
+
+echo "delete old folders"
 for f in *baseline_results*; do
-  echo $f
-  echo "${f//\\//}"
   if [ "$f" != '*.log' ]
   then
-    mkdir -p "${f//\\//}"
+    echo $f
   fi
-  mv -- "$f"/* "${f//\\//}"
 done
 
-for f in *baseline_results*.log*; do
+echo "change log file names"
+for f in $(find . -type f -name '*.log*'); do
   echo $f
-  mv -- "$f" "${f//\\//}"
 done
