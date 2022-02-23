03c847a3a6404248d3b91c5545362d51771a8acb
Idan-Golan <idan.go@campus.technion.ac.il>
2022-02-23 18:46:57 +0200

    Add run scripts for asymmetric data-type for the 1st conv layer

---
+++ run_alexnet_asym_1st_conv.sh
@@ -0,0 +1,4 @@
+python3 main.py --action TRAINING --arch alexnetAsym-cifar100 --epoch 100 --batch_size 128 --seed 44 --gpu 1 --compute_flavour 4 --v 1
+python3 main.py --action TRAINING --arch alexnetAsym-cifar100 --epoch 100 --batch_size 128 --seed 44 --gpu 1 --compute_flavour 5 --v 1
+python3 main.py --action TRAINING --arch alexnetAsym-cifar100 --epoch 100 --batch_size 128 --seed 44 --gpu 1 --compute_flavour 6 --v 1
+python3 main.py --action TRAINING --arch alexnetAsym-cifar100 --epoch 100 --batch_size 128 --seed 44 --gpu 1 --compute_flavour 9 --v 1
