b1102a4d433e8219729b7988e6b3aee00eef559a
Idan Golan <IdanGolan7@gmail.com>
2022-03-02 22:13:36 +0200

    Fix special matmuls for bwd passes & analyze the impact of grad_output conversion.

--- SpecialMatmul_bwd_igrad.py
+++ SpecialMatmul_bwd_igrad.py
@@ -20,13 +20,14 @@ class BF9Matmul_bwd_igrad(Function):
 
             inputs, weights = ctx.saved_tensors
 
-            weights_bf12 = convert_to_bf12(weights)
-            inputs_bf9 = convert_to_bf9(inputs)
+            weights_bf9 = convert_to_bf9(weights)
+            inputs_bf12 = convert_to_bf12(inputs)
             grad_output_bf12 = convert_to_bf12(grad_output.contiguous())
+            grad_output_bf9 = convert_to_bf9(grad_output.contiguous())
 
-            inputs_gradients = grad_output_bf12.matmul(weights_bf12)
+            inputs_gradients = grad_output_bf9.matmul(weights_bf9)
             grad_output_new = grad_output_bf12.transpose(1,2).contiguous()
-            weights_gradients = grad_output_new.matmul(inputs_bf9)
+            weights_gradients = grad_output_new.matmul(inputs_bf12)
             weights_gradients = weights_gradients.sum(0)
             return inputs_gradients, weights_gradients
 
@@ -47,12 +48,13 @@ class BF10Matmul_bwd_igrad(Function):
 
             inputs, weights = ctx.saved_tensors
 
-            weights_bf12 = convert_to_bf12(weights)
-            inputs_bf10 = convert_to_bf10(inputs)
+            weights_bf10 = convert_to_bf10(weights)
+            inputs_bf12 = convert_to_bf12(inputs)
             grad_output_bf12 = convert_to_bf12(grad_output.contiguous())
+            grad_output_bf10 = convert_to_bf10(grad_output.contiguous())
 
-            inputs_gradients = grad_output_bf12.matmul(weights_bf12)
+            inputs_gradients = grad_output_bf10.matmul(weights_bf10)
             grad_output_new = grad_output_bf12.transpose(1,2).contiguous()
-            weights_gradients = grad_output_new.matmul(inputs_bf10)
+            weights_gradients = grad_output_new.matmul(inputs_bf12)
             weights_gradients = weights_gradients.sum(0)
             return inputs_gradients, weights_gradients
