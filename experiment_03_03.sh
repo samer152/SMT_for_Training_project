fb0e1bb0e42678350d9d47bcc62f6c8c53c9343a
Tuval198 <tuvalgelvan@campus.technion.ac.il>
2022-03-03 18:40:37 +0200

    update running script

--- experiment_03_03.sh
+++ experiment_03_03.sh
@@ -27,10 +27,10 @@
 ## ==============================================================================
 #
 ## ==============================================================================
-echo "Backward"
+#echo "Backward"
 #
-echo "Mantissa 2"
-python3 main.py --action TRAINING --arch resnet18-cifar100 --epoch 150 --batch_size 128 --seed 345 --compute_flavour 2 --v 1  --WD 5e-6 --GAMMA 0.1 --MOMENTUM 0.5 --distributed 0 --gpu 0 --exp backward --dir backward
+#echo "Mantissa 2"
+#python3 main.py --action TRAINING --arch resnet18-cifar100 --epoch 150 --batch_size 128 --seed 345 --compute_flavour 2 --v 1  --WD 5e-6 --GAMMA 0.1 --MOMENTUM 0.5 --distributed 0 --gpu 0 --exp backward --dir backward
 #
 #echo "Mantissa 0"
 #python3 main.py --action TRAINING --arch resnet18-cifar100 --epoch 150 --batch_size 128 --seed 345 --compute_flavour 1 --v 1  --WD 5e-6 --GAMMA 0.1 --MOMENTUM 0.5 --distributed 0 --gpu 0 --exp backward --dir backward
