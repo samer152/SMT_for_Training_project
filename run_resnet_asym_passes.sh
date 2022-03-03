76ffe40d4170c34071c3d55c12db0bc6c56431e1
Idan-Golan <idan.go@campus.technion.ac.il>
2022-03-03 10:49:53 +0200

    Add results using symmetric matmul on asymmetric bwd passes.

--- run_resnet_asym_passes.sh
+++ run_resnet_asym_passes.sh
@@ -1,6 +1,6 @@
-python3 main.py --action TRAINING --arch resnet18-cifar100 --epoch 200 --batch_size 128 --seed 44 --gpu 0 --compute_flavour 20 --v 1
-python3 main.py --action TRAINING --arch resnet18-cifar100 --epoch 200 --batch_size 128 --seed 44 --gpu 0 --compute_flavour 21 --v 1
-python3 main.py --action TRAINING --arch resnet18-cifar100 --epoch 200 --batch_size 128 --seed 44 --gpu 0 --compute_flavour 22 --v 1
-python3 main.py --action TRAINING --arch resnet18-cifar100 --epoch 200 --batch_size 128 --seed 44 --gpu 0 --compute_flavour 23 --v 1
-python3 main.py --action TRAINING --arch resnet18-cifar100 --epoch 200 --batch_size 128 --seed 44 --gpu 0 --compute_flavour 24 --v 1
-python3 main.py --action TRAINING --arch resnet18-cifar100 --epoch 200 --batch_size 128 --seed 44 --gpu 0 --compute_flavour 25 --v 1
+#python3 main.py --action TRAINING --arch resnet18-cifar100 --epoch 100 --batch_size 128 --seed 44 --gpu 0 --compute_flavour 20 --v 1
+#python3 main.py --action TRAINING --arch resnet18-cifar100 --epoch 100 --batch_size 128 --seed 44 --gpu 0 --compute_flavour 21 --v 1
+python3 main.py --action TRAINING --arch resnet18-cifar100 --epoch 100 --batch_size 128 --seed 44 --gpu 0 --compute_flavour 22 --v 1
+python3 main.py --action TRAINING --arch resnet18-cifar100 --epoch 100 --batch_size 128 --seed 44 --gpu 0 --compute_flavour 23 --v 1
+python3 main.py --action TRAINING --arch resnet18-cifar100 --epoch 100 --batch_size 128 --seed 44 --gpu 0 --compute_flavour 24 --v 1
+python3 main.py --action TRAINING --arch resnet18-cifar100 --epoch 100 --batch_size 128 --seed 44 --gpu 0 --compute_flavour 25 --v 1
