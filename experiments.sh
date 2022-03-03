7c98822878f76bb90497189f55adef7362babcf2
Tuval198 <tuvalgelvan@campus.technion.ac.il>
2022-03-03 13:08:23 +0200

    update directories

--- experiments.sh
+++ experiments.sh
@@ -5,19 +5,19 @@
 #
 echo "Backward"
 echo "Baseline - Mantissa 23"
-python3 main.py --action TRAINING --arch resnet18-cifar100 --epoch 200 --batch_size 128 --seed 345 --compute_flavour 0 --v 1  --WD 5e-6 --GAMMA 0.1 --MOMENTUM 0.5 --distributed 0 --gpu 1
+python3 main.py --action TRAINING --arch resnet18-cifar100 --epoch 200 --batch_size 128 --seed 345 --compute_flavour 0 --v 1  --WD 5e-6 --GAMMA 0.1 --MOMENTUM 0.5 --distributed 0 --gpu 0
 
 #echo "Mantissa 7"
 #python3 main.py --action TRAINING --arch resnet18-cifar100 --epoch 200 --batch_size 128 --seed 345 --compute_flavour 4 --v 1  --WD 5e-6 --GAMMA 0.1 --MOMENTUM 0.5 --distributed 0 --gpu 1
 
 echo "Mantissa 4"
-python3 main.py --action TRAINING --arch resnet18-cifar100 --epoch 200 --batch_size 128 --seed 345 --compute_flavour 3 --v 1 --WD 5e-6 --GAMMA 0.1 --MOMENTUM 0.5 --distributed 0 --gpu 1
+python3 main.py --action TRAINING --arch resnet18-cifar100 --epoch 200 --batch_size 128 --seed 345 --compute_flavour 3 --v 1 --WD 5e-6 --GAMMA 0.1 --MOMENTUM 0.5 --distributed 0 --gpu 0
 
 echo "Mantissa 2"
-python3 main.py --action TRAINING --arch resnet18-cifar100 --epoch 200 --batch_size 128 --seed 345 --compute_flavour 2 --v 1  --WD 5e-6 --GAMMA 0.1 --MOMENTUM 0.5 --distributed 0 --gpu 1
+python3 main.py --action TRAINING --arch resnet18-cifar100 --epoch 200 --batch_size 128 --seed 345 --compute_flavour 2 --v 1  --WD 5e-6 --GAMMA 0.1 --MOMENTUM 0.5 --distributed 0 --gpu 0
 
 echo "Mantissa 0"
-python3 main.py --action TRAINING --arch resnet18-cifar100 --epoch 200 --batch_size 128 --seed 345 --compute_flavour 1 --v 1  --WD 5e-6 --GAMMA 0.1 --MOMENTUM 0.5 --distributed 0 --gpu 1
+python3 main.py --action TRAINING --arch resnet18-cifar100 --epoch 200 --batch_size 128 --seed 345 --compute_flavour 1 --v 1  --WD 5e-6 --GAMMA 0.1 --MOMENTUM 0.5 --distributed 0 --gpu 0
 
 #echo "Custom Layer Training"
 #echo "Mantissa 0"
