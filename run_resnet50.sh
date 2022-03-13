a6a572a1a94a1e0525f80a260d5cff5cc2064471
Idan <idan.go@campus.technion.ac.il>
2022-03-13 07:58:20 +0200

    Add base results for resnet50

---
+++ run_resnet50.sh
@@ -0,0 +1,7 @@
+python3 main.py --action TRAINING --arch resnet50-cifar100 --epoch 100 --batch_size 128 --seed 345 --gpu 0 --compute_flavour 2 --v 1 --MOMENTUM 0.7 --GAMMA 0.3 --WD 2e-4 --MILESTONES 30 60 90 --LRD 1 --LR 0.1
+python3 main.py --action TRAINING --arch resnet50-cifar100 --epoch 100 --batch_size 128 --seed 345 --gpu 0 --compute_flavour 4 --v 1 --MOMENTUM 0.7 --GAMMA 0.3 --WD 2e-4 --MILESTONES 30 60 90 --LRD 1 --LR 0.1
+python3 main.py --action TRAINING --arch resnet50-cifar100 --epoch 100 --batch_size 128 --seed 345 --gpu 0 --compute_flavour 5 --v 1 --MOMENTUM 0.7 --GAMMA 0.3 --WD 2e-4 --MILESTONES 30 60 90 --LRD 1 --LR 0.1
+python3 main.py --action TRAINING --arch resnet50-cifar100 --epoch 100 --batch_size 128 --seed 345 --gpu 0 --compute_flavour 6 --v 1 --MOMENTUM 0.7 --GAMMA 0.3 --WD 2e-4 --MILESTONES 30 60 90 --LRD 1 --LR 0.1
+python3 main.py --action TRAINING --arch resnet50-cifar100 --epoch 100 --batch_size 128 --seed 345 --gpu 0 --compute_flavour 7 --v 1 --MOMENTUM 0.7 --GAMMA 0.3 --WD 2e-4 --MILESTONES 30 60 90 --LRD 1 --LR 0.1
+python3 main.py --action TRAINING --arch resnet50-cifar100 --epoch 100 --batch_size 128 --seed 345 --gpu 0 --compute_flavour 8 --v 1 --MOMENTUM 0.7 --GAMMA 0.3 --WD 2e-4 --MILESTONES 30 60 90 --LRD 1 --LR 0.1
+python3 main.py --action TRAINING --arch resnet50-cifar100 --epoch 100 --batch_size 128 --seed 345 --gpu 0 --compute_flavour 9 --v 1 --MOMENTUM 0.7 --GAMMA 0.3 --WD 2e-4 --MILESTONES 30 60 90 --LRD 1 --LR 0.1
